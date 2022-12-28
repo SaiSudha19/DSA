860c157c7500e3376242629471c56251d9b3af24
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2022-12-28 20:04:38 +0530

    Create selection_sort.cpp

---
+++ selection_sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+void selectSort(int arr[],int n)
+{
+    
+    for(int i=0;i<n-1;i++)
+    {
+        int min_ind=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[min_ind])
+            min_ind=j;
+        }
+        swap(arr[m-----in_ind],arr[i]);
+        
+    }
+    
+}
+int main()
+{
+    int n=4;
+    int arr[]={1,4,2,3};
+    selectSort(arr,n);
+    for(int i=0;i<n;i++)
+    cout<<arr[i];
+    return 0;
+}
