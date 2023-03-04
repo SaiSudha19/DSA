349c83bf4172de10ed8f26031e02676728161aa5
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-03-04 12:46:56 +0530

    Create delete_first_node_in_singly_linkedlist.cpp

---
+++ delete_first_node_in_singly_linkedlist.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+void printlist(node *head)
+{
+    node *curr=head;
+    while(curr!=NULL)
+    {
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+    
+}
+
+node *deletehead(node *head)
+{
+    if(head==NULL)
+    return NULL;
+    else
+    {
+        node *temp=head;
+        head=head->next;
+        delete temp;
+        return head;
+    }
+}
+
+int main()
+{
+    node *head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    printlist(head);
+    cout<<endl;
+    head=deletehead(head);
+    printlist(head);
+    return 0;
+}
