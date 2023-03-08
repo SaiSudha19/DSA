da54cadb69cd9177e92293c2d5f8eaa329846d3a
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-03-08 18:35:27 +0530

    Create circular_linkedlist_traversal(do_while_loop).cpp

---
+++ circular_linkedlist_traversal(do_while_loop).cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+void printlist(node *head)
+{
+    if(head==NULL)
+    return;
+    node *p=head;
+    do{
+        cout<<p->data<<" ";
+        p=p->next;
+    }while(p!=head);
+}
+
+int main()
+{
+    node *head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    head->next->next->next->next=head;
+    printlist(head);
+    return 0;
+}
+
+
