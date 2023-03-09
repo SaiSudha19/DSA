610eceb042335d23a85c3ec04cc6b57bc88ef223
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-03-09 15:32:16 +0530

    Create insert_at_begining_of_circular_linkedlist(naive).cpp

---
+++ insert_at_begining_of_circular_linkedlist(naive).cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+void printlist(node *head)
+{
+    if(head==NULL)
+    return;
+    node *p=head;
+    do{
+        cout<<p->data<<" ";
+        p=p->next;
+    }while(p!=head);
+}
+
+node *insertbegin(node *head,int x)
+{
+    node *temp=new node(x);
+    if(head==NULL)
+    return temp;
+    else
+    {
+        node *curr=head;
+        while(curr->next!=head)
+        curr=curr->next;
+        curr->next=temp;
+        temp->next=head;
+    }
+    return temp;
+}
+
+int main()
+{
+    node *head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    head->next->next->next->next=head;
+    printlist(head);
+    head=insertbegin(head,5);
+    cout<<endl;
+    printlist(head);
+    return 0;
+}
+
+
+
