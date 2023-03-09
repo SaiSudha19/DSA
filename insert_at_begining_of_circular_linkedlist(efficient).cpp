715791c04ec74acb7784aceb68426973bcedd2d2
Sudha <93865988+SaiSudha19@users.noreply.github.com>
2023-03-09 15:33:13 +0530

    Create insert_at_begining_of_circular_linkedlist(efficient).cpp

---
+++ insert_at_begining_of_circular_linkedlist(efficient).cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+void printlist(node *head)
+{
+    if(head==NULL)
+    return;
+    node *p=head;
+    do{
+        cout<<p->data<<" ";
+        p=p->next;
+    }while(p!=head);
+}
+
+node *insertbegin(node *head,int x)
+{
+    node *temp=new node(x);
+    if(head==NULL)
+    return temp;
+    else
+    {
+        temp->next=head->next;
+        head->next=temp;
+        int t=temp->data;
+        temp->data=head->data;
+        head->data=t;
+        return head;
+    }
+}
+
+int main()
+{
+    node *head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    head->next->next->next->next=head;
+    printlist(head);
+    head=insertbegin(head,5);
+    cout<<endl;
+    printlist(head);
+    return 0;
+}
+
+
+
